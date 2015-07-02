46af564c57d923a5dc12a2214fb190d2f2f639cf
Daniel Pabon <daniel.epm12@gmail.com>
2015-07-01 22:18:06 -0500

    change minors

--- R/tracer.R
+++ R/tracer.R
@@ -1,7 +1,7 @@
-cadena.1 <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/corrida_1/cadena.1.trace")
+cadena.1 <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/cadena.1.trace")
 Sys.sleep(10*60)
-cadena.1p <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/corrida_1/cadena.1.trace")
-cadena.2<- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/corrida_1/cadena.2.trace")
+cadena.1p <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/cadena.1.trace")
+cadena.2<- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/cadena.2.trace")
 x11()
 par(mfrow=c(2,1))
 if (cadena.1==cadena.1p){
@@ -11,10 +11,10 @@ if (cadena.1==cadena.1p){
   Sys.sleep(30)
 } else {
   while(cadena.1 != cadena.1p){
-    cadena.1 <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/corrida_1/cadena.1.trace")
+    cadena.1 <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/cadena.1.trace")
     Sys.sleep(10*60)
-    cadena.1p <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/corrida_1/cadena.1.trace")
-    cadena.2<- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/corrida_1/cadena.2.trace")
+    cadena.1p <- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/cadena.1.trace")
+    cadena.2<- read.delim("~/Copy/proyecto/data/raw/prueba_piloto/bayes/cadena.2.trace")
     plot (cadena.1p$X.cycle, cadena.1p$allocent, type="l", xlab="Cycle", ylab="Allocent", main="Cadena 1" )
     plot (cadena.2$X.cycle, cadena.2$allocent, type="l", xlab="Cycle", ylab="Allocent", main="Cadena 2" )
   }
@@ -24,7 +24,7 @@ if (cadena.1==cadena.1p){
 #?Sys.sleep
 
 #head(cadena.1)
-#plot(cadena.1)
+plot(cadena.1)
 #x11()
 #par(mfrow=c(2,1))
 #plot(cadena.1$X.cycle, cadena.1$alpha, type="l") 
