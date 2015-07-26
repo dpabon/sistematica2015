0ef998f51674f8778752f8777ab71c7178a6b7c2
Daniel Pabon <daniel.epm12@gmail.com>
2015-07-26 17:55:46 -0500

    Trace Scripts

---
+++ R/trace_bayes_exploratory.R
@@ -0,0 +1,9 @@
+#####################################################################################
+##                  Trace of MrBayes                                              ##
+#####################################################################################
+
+total.nex.run1 <- read.delim("~/Copy/proyecto/data/processed/mrbayes/total.nex.run1.p", header=T)
+total.nex.run2<- read.delim("~/Copy/proyecto/data/")
+View(head(total.nex.run1))
+plot(total.nex.run1$Gen, total.nex.run1$TL.all., type="l")
+total.nex.
