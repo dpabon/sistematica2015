eb1a7ff5ad736485b87e22381783459d9565c2b1
Daniel Pabon <daniel.epm12@gmail.com>
2015-07-26 18:03:40 -0500

    update graphic

--- R/trace_bayes_exploratory.R
+++ R/trace_bayes_exploratory.R
@@ -3,7 +3,11 @@
 #####################################################################################
 
 total.nex.run1 <- read.delim("~/Copy/proyecto/data/processed/mrbayes/total.nex.run1.p", header=T)
-total.nex.run2<- read.delim("~/Copy/proyecto/data/")
+total.nex.run2<- read.delim("~/Copy/proyecto/data/processed/mrbayes/total.nex.run2.p", header = T)
 View(head(total.nex.run1))
-plot(total.nex.run1$Gen, total.nex.run1$TL.all., type="l")
-total.nex.
+
+plot(total.nex.run1$Gen, total.nex.run1$TL.all., type="l", xlab="Generation", ylab = "Tree Lenght", main = "Exploratory Analysis
+     Chain 1" )
+plot(total.nex.run2$Gen, total.nex.run2$TL.all., type="l", xlab="Generation", ylab = "Tree Lenght", main = "Exploratory Analysis
+     Chain 2")
+par(mfrow=c(2,1))
